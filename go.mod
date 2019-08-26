cf0911d361d4d60315f95c75cf04978bf5702297
masoud <masoud@ghiasi.me>
2019-08-26 18:44:44 +0430

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ghiac/smpp-proxy
+
+go 1.12
+
+require (
+	github.com/ghiac/go-smpp v0.0.0-20190826140313-89830b71480f
+	github.com/pborman/uuid v1.2.0 // indirect
+)
---
+++ main_client.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/ghiac/go-smpp"
+	"time"
+)
+
+func main() {
+	token := "11aa4083186e67008b483412ca7238d184b538a7"
+	temp := go_smpp.NewClient("localhost:7878", "admin", "admin")
+	temp.Start()
+	des := "898625337"
+	var parameters []go_smpp.Parameter
+	parameters = append(parameters, go_smpp.NewParameter(1, []byte(token)))
+	time.Sleep(1000 * time.Millisecond)
+	temp.Send("userId", des, "msg1", parameters)
+	time.Sleep(1000 * time.Millisecond)
+	temp.Send("aa", des, "msg2", parameters)
+	time.Sleep(1000 * time.Millisecond)
+	temp.Send("aa", des, "msg3", parameters)
+	//<-signal.Wait()
+}
